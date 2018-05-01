15a672222b96b56b91bf24fe76b2708440fbe236
guillermoLeon26 <gleon2611@gmail.com>
2018-04-30 21:10:55 -0500

    inicio menus

---
+++ resources/views/plantilla/include/menuPrincipal.blade.php
@@ -0,0 +1,43 @@
+<aside class="main-sidebar">
+    <!-- sidebar: style can be found in sidebar.less -->
+    <section class="sidebar">
+      <!-- Sidebar user panel -->
+      
+      <!-- sidebar menu: : style can be found in sidebar.less -->
+      <ul class="sidebar-menu">
+        <li class="header">MENU</li>
+        
+        <li class="treeview">
+          <a href="{{ url('menu') }}">
+            <i class="glyphicon glyphicon-th-list"></i> <span>Menus</span>
+          </a>
+        </li>
+
+        <li class="treeview">
+          <a href="#">
+            <i class="glyphicon glyphicon-book"></i><span>Pedidos</span>
+          </a>
+        </li>
+        
+        <!--
+        <li class="treeview">
+          <a href="#">
+            <i class="fa fa fa-gear"></i>
+            <span>Configuración</span>
+            <span class="pull-right-container">
+              <i class="fa fa-angle-left pull-right"></i>
+            </span>
+          </a>
+          <ul class="treeview-menu">
+            <li><a href="{{ url('configuracion/sucursal') }}"><i class="fa fa-building-o"></i> Sucursales</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i> Morris</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i> Flot</a></li>
+            <li><a href="#"><i class="fa fa-circle-o"></i> Inline charts</a></li>
+          </ul>
+        </li>
+      -->
+        
+      </ul>
+    </section>
+    <!-- /.sidebar -->
+  </aside>
